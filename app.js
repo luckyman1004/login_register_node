8e46ffbabaa083d42aff11fa67e4ffc58d1ab3f6
luckyman1004 <luckysuperdev@gmail.com>
2019-11-11 10:47:02 +0530

    Update

--- app.js
+++ app.js
@@ -3,3 +3,15 @@ const path = require ('path');
 
 const app = express();
 app.use(express.urlencoded({extended : false}));
+
+app.use(express.static(path.join(__dirname,'public')));
+
+app.set('views',path.join(__dirname,'views'));
+app.set('view engine','pug');
+
+
+app.listen(3000 ,()=>{
+    console.log("server is running on port 3000");
+});
+
+module.exports = app;
