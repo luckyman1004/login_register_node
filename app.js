2b872a49252e4e74e3fb069c78992d716ba2bd45
luckyman1004 <luckysuperdev@gmail.com>
2019-11-13 09:20:35 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require ('express');
 const path = require ('path');
-
+const pageRouter = require ('./routes/pages');
 const app = express();
+
 app.use(express.urlencoded({extended : false}));
 
 app.use(express.static(path.join(__dirname,'public')));
@@ -9,9 +10,7 @@ app.use(express.static(path.join(__dirname,'public')));
 app.set('views',path.join(__dirname,'views'));
 app.set('view engine','pug');
 
-app.get('/', function (req , res) {
-    res.render ('index');
-});
+app.use('/', pageRouter);
 
 app.listen(3000 ,()=>{
     console.log("server is running on port 3000");
